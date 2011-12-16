96d485493a80cc8b72ff6b1471ae25e4ec3adb00
nil paulownia <hisary+dev@gmail.com>
2011-12-16 11:35:15 +0900

    remove atom.rb

--- lib/atom.rb
+++
@@ -1,8 +0,0 @@
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
-
-module Atom
-  VERSION = '0.0.1'
-end
-
-require 'atom/feed'
