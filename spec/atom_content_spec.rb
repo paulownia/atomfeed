72924537446f4965b53c14857ad18f9e9dab9aef
hisanori <hisary@gmail.com>
2020-11-20 02:05:20 +0900

    fix spec

--- spec/atom_content_spec.rb
+++ spec/atom_content_spec.rb
@@ -1,17 +1,17 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
 describe Atom::Content, "をnewした時、" do
   before do
     @content = Atom::Content.new
   end
-  
+
   it "contentのデフォルトのtypeは text で、" do
-    @content.type.should == "text"
+    expect(@content.type).to eq("text")
   end
- 
+
   it "to_elementしたとき、contentのtextが無ければnil" do
-    @content.to_element.should be_nil
+    expect(@content.to_element).to be nil
   end
 end
 
@@ -21,19 +21,29 @@ describe Atom::Content, "のtextをセットした時、" do
     @content.text = "<p>hogehoge</p>"
   end
 
+  it "to_sするとAtomのcontent要素の内容はテキストとして出力され、タグ等が実体参照に置き換えられている。" do
+    expect(@content.to_s).to eq(
+      "<content type='text'>&lt;p&gt;hogehoge&lt;/p&gt;</content>"
+    )
+  end
+
   it "typeがxhtmlならば、Atomのcontent要素の内容はxhtmlで出力され、" do
     @content.type = "xhtml"
-    @content.to_s.should == 
+
+    expect(@content.to_s).to eq(
       "<content type='xhtml'>" +
-      "<html xmlns='http://www.w3.org/1999/xhtml'>" + 
+      "<html xmlns='http://www.w3.org/1999/xhtml'>" +
       "<p>hogehoge</p>" +
       "</html>" +
       "</content>"
+    )
   end
-  
+
   it "typeをhtmlに変えると、Atomのcontent要素の内容はタグ等が実体参照に置き換えられたhtmlで出力される。" do
     @content.type = "html"
-    @content.to_s.should == 
-      "<content type='html'>&lt;p&gt;hogehoge&lt;/p&gt;</content>" 
+
+    expect(@content.to_s).to eq(
+      "<content type='html'>&lt;p&gt;hogehoge&lt;/p&gt;</content>"
+    )
   end
 end
